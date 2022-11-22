30c8fcbc771f90c1d0bc379162ac66351816a24c
Hancer-byte <66197354+Hancer-byte@users.noreply.github.com>
2022-11-21 19:29:35 -0600

    Create Act 1.3.c

---
+++ Actividad 1.3/Act 1.3.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include "omp.h"
+#define NUM_THREADS 
+
+int main(){
+	double t1, t2, tiempo;
+	const double startTime = omp_get_wtime();
+	t1 = omp_get_wtime();
+	omp_set_num_threads(NUM_THREADS);
+	#pragma omp parallel
+	{
+		int ID = omp_get_thread_num();
+		printf("Procesadores(%d)", ID);
+		printf("Multiples(%d)", ID);
+		printf("en accion(%d) --- %d\n", ID, ID);
+	}
+t2=omp_get_wtime();
+tiempo=t2-t1;
+printf("\nLe tomó (%lf) segundos\n", tiempo);
+}
